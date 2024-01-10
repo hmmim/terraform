5740cd6dd59b9fccd5ce23c61efc80a8a757ba02
hmmim <119211030+hmmim@users.noreply.github.com>
2024-01-10 21:41:22 +0700

    Update variables.tf

---
+++ modules/database/variables.tf
@@ -0,0 +1,30 @@
+variable "db_allocated_storage" {
+
+}
+variable "db_instance_class" {
+  
+}
+variable "identifier" {
+  
+}
+variable "db_skip_snapshot" {
+  
+}
+variable "db_name" {
+  
+}
+variable "db_subnet_group_name" {
+  
+}
+variable "db_user" {
+  
+}
+variable "db_engine_version" {
+  
+}
+variable "rds_sg" {
+  
+}
+variable "db_password" {
+  
+}
