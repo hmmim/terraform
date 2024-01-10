a4fdf4ea0351ae4803f83fb18310f50b35ce1306
hmmim <119211030+hmmim@users.noreply.github.com>
2024-01-10 21:41:01 +0700

    Update main.tf

---
+++ modules/database/main.tf
@@ -0,0 +1,17 @@
+resource "aws_db_instance" "three_tier_db" {
+    allocated_storage    = var.db_allocated_storage
+    instance_class       = var.db_instance_class
+    engine               = "mysql"
+    engine_version       = var.db_engine_version
+    db_name              = var.db_name
+    username             = var.db_user
+    password             = var.db_password
+    db_subnet_group_name = var.db_subnet_group_name
+    identifier           = var.identifier
+    skip_final_snapshot  = var.db_skip_snapshot
+    vpc_security_group_ids = var.rds_sg
+
+    tags = {
+        Name = "three_tier_db"
+    }
+}
